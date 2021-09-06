a38c66358cb1889aef51ab05470c01976b973190
sidlogin <siddhartha.gupta@gmail.com>
2021-09-05 22:19:38 -0500

    module.exports examples

--- basics/notes-app/app.js
+++ basics/notes-app/app.js
@@ -1,7 +1,9 @@
 // const fs = require('fs');
-const add = require('./utils');
+// const add = require('./utils');
 const getNotes = require('./notes');
 
-console.log('App.js', add(10, 10));
+// console.log('App.js', add(10, 10));
 
-console.log(getNotes());
+const msg = getNotes();
+
+console.log(msg);
