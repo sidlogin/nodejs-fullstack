1f40e1873bfff0c9fbc335a1f2c7fa2b7b123bbb
sidlogin <siddhartha.gupta@gmail.com>
2021-09-06 13:37:30 -0500

    Updating reading & riting JSON data file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
 
-const book = {
-     title: 'Ego is the enemy',
-     author: 'Ryan Holiday'
-};
-
-const bookJSON = JSON.stringify(book);
-
-fs.writeFileSync('1-json.json', bookJSON);
-
 const dataBuffer = fs.readFileSync('1-json.json');
-console.log(dataBuffer);
-
 const dataJSON  = dataBuffer.toString();
 const data = JSON.parse(dataJSON)
+
+data.title = 'Update JSON File';
+data.author = 'Sid Gupta';
+
+const updatedJSON = JSON.stringify(data);
+fs.writeFileSync('1-json.json', updatedJSON);
 
 console.log(data);
 
