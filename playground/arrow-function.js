13b85c55c7bbec06c9323adc8f8939256f8ee733
sidlogin <siddhartha.gupta@gmail.com>
2021-09-06 15:14:11 -0500

    types of arrow functions

---
+++ playground/arrow-function.js
@@ -0,0 +1,48 @@
+const square = function (x) {
+     return x * x;
+};
+
+const squareArrowFn1 = (x) => {
+     return x * x;
+}
+
+const squareArrowFn2 = (x) => x * x;
+
+console.log(square(2));
+console.log(squareArrowFn1(3));
+console.log(squareArrowFn2(4));
+
+const event = {
+     name: 'Arrow Function with Object',
+     guestList: ['Gaurav', 'Naveen', 'Nikhil'],
+     printTitle: function() {
+          console.log('Name of Title: ', this.name);
+     },
+     printGuestList() {
+          this.guestList.forEach((guest) => {
+               console.log(`${guest} is attending party!`);
+          });
+     }
+
+};
+event.printTitle();
+event.printGuestList();
+
+const tasks = {
+     tasks: [{
+         text: 'Grocery shopping',
+         completed: true
+     },{
+         text: 'Clean yard',
+         completed: false
+     }, {
+         text: 'Film course',
+         completed: false
+     }],
+     getTasksToDo() {
+          return this.tasks.filter(task => !task.completed);
+     }
+ };
+
+ console.log(tasks.getTasksToDo());
+
