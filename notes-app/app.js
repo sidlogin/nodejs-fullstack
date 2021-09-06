069656168afa71f6ee1df87a75f54415d837bcbd
sidlogin <siddhartha.gupta@gmail.com>
2021-09-06 11:51:34 -0500

    Node JS Reading Commandline Arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,19 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 
 const msg = getNotes();
+const command = process.argv[2];
 
 console.log(msg);
 console.log(validator.isEmail('siddharth@gmail.com'));
+console.log(chalk.red.inverse.bold('Success!'));
+
+console.log('Commandline Arguments: ', process.argv);
+
+if (command === 'add') {
+     console.log('Adding Note!');
+} else if (command === 'remove') {
+     console.log('Deleting Note!');
+}
