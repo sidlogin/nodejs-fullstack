74e7ddc8ebe2cd4952f55760ac7120e87b1b5260
sidlogin <siddhartha.gupta@gmail.com>
2021-09-07 07:56:10 -0500

    Object Destructuring in details

--- playground/objects.js
+++ playground/objects.js
@@ -7,25 +7,30 @@ const product = {
      salePrice: null,
 };
 
-// const {label, stock} = product;
-// console.log(label, stock);
+const {label, stock} = product;
+console.log('Example1: ', label, stock);
 
-// const {label: productLabel, stock, rating} = product;
-// console.log(label, stock, rating);
+const product1 = { label1: 'Red Book', price1: 5, stock1: 200, salePrice1: null };
+const { label1: productLabel1, stock1, rating1 } = product1;
 
-// const {label: productLabel, stock, rating = 5 } = product;
-// console.log(label, stock, rating);
+// Expected output: rating1 will be defined as it is not exist.
+console.log('Example2: ', productLabel1, stock1, rating1);
 
-// const new_product = {
-//      ...product, ...{ rating: '4.5' }
-// };
+const product2 = { label2: 'Red Book', price2: 5, stock2: 200, salePrice2: null };
+const { label2: productLabel2, stock2, rating2 = 5 } = product2;
 
-// const {label: productLabel, stock, rating = 5 } = new_product;
-// console.log(productLabel, stock, rating);
+// Expected output: rating2 will be return default value 5 as it is not exist in object.
+console.log('Example3: ', productLabel2, stock2, rating2);
 
-const transaction = ((type, { label, stock }) => {
-     console.log(type, label, stock);
+const new_product = { ...product2, ...{ rating3: '4.5' } };
+const {label2: productLabel3, stock3, rating3 = 5 } = new_product;
+
+// Expected output: stock3 will be return undefined & raiting3 would be 4.5 as it is not exist in object.
+console.log('Example4: ', productLabel3, stock3, rating3);
+
+const transaction = ((type, { label2, stock2 }) => {
+     console.log('Example5: ', type, label2, stock2);
 });
 
-transaction('order', product);
+transaction('order', product2);
 
