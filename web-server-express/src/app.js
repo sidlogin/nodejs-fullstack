cdc7d2563b30a2f800e3b9770249a71e7877607c
sidlogin <siddhartha.gupta@gmail.com>
2021-09-07 19:20:31 -0500

    Deleting static files

--- web-server-express/src/app.js
+++ web-server-express/src/app.js
@@ -1,21 +1,41 @@
 const path = require('path');
 const express = require('express');
 
-// console.log(__dirname);
-// console.log(path.join(__dirname, '../public'));
-
 const PUBLIC_DIRECTORY_PATH = path.join(__dirname, '../public');
-const App = express();
+const app = express();
 
-App.use(express.static(PUBLIC_DIRECTORY_PATH))
+app.use(express.static(PUBLIC_DIRECTORY_PATH));
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
 
-App.get('/weather', (req, res) => {
+app.get('/weathers', (req, res) => {
+     res.render('index', {
+          title: 'Weather App',
+          name: 'Siddharth Gupta'
+     });
+});
+
+app.get('/about', (req, res) => {
+     res.render('about', {
+          title: 'About ExpressJS App',
+          name: 'Siddharth Gupta'
+     });
+});
+
+app.get('/help', (req, res) => {
+     res.render('about', {
+          title: 'ExpressJS Help',
+          name: 'Siddharth Gupta'
+     });
+});
+
+app.get('/weather', (req, res) => {
      res.send({
           forecast: 'Rain is expected',
           location: 'Dallas'
      });
 });
 
-App.listen(3000, () => {
+app.listen(3000, () => {
      console.log('Express Server started on http://localhost:3000');
 })
